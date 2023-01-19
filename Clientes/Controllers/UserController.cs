b301940fb61b573168831eae51679cbc623f3754
Ivan Gonzalo Bonfigli <system04@live.com.ar>
2023-01-19 16:29:29 -0300

    Migracion .net core 3.1

--- Clientes/Controllers/UserController.cs
+++ Clientes/Controllers/UserController.cs
@@ -1,16 +1,9 @@
 using System;
-using System.IO;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Clientes.Models;
-using System.Security.Cryptography.X509Certificates;
 using Clientes.Services;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.DependencyInjection;
+
 
 
 namespace User.Controllers
